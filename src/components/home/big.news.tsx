9c9bfe06801bbc7eb7b11e030862d7b81fe84230
Vaibhav Kaushik <120114579+vibhu2208@users.noreply.github.com>
2024-12-14 02:09:08 +0530

    Update big.news.tsx

--- src/components/home/big.news.tsx
+++ src/components/home/big.news.tsx
@@ -3,13 +3,13 @@ import React from 'react'
 
 const BigNews = () => {
     return (
-        <div className='flex flex-col gap-8 p-8 md:p-16 text-white' style={{backgroundImage: 'url("/images/Types_of_Artificial_Intelligence.avif")', backgroundSize: "100%", objectFit: "cover", backgroundPosition: "center"}}>
-            <div className='md:w-[50%] w-full flex flex-col gap-10 md:p-8'>
+        <div className='flex flex-col gap-8 p-8 md:p-16 text-white' style={{backgroundImage: 'url("/images/bignews-fourth.png")', backgroundSize: "cover", objectFit: "cover", backgroundPosition: "center"}}>
+            <div className='md:w-[50%] text-black w-full flex flex-col gap-10 md:p-8 ml-auto'>
                 <h2 className='md:text-6xl text-4xl font-bold' data-aos="fade-up" data-aos-delay="300">Big news!</h2>
-                <h2 className='md:text-5xl text-3xl font-medium' data-aos="fade-up" data-aos-delay="300">We are thrilled to announce the release of  Generative AI:</h2>
+                <h2 className='md:text-5xl text-3xl font-medium' data-aos="fade-up" data-aos-delay="300">We are thrilled to announce the release of Generative AI:</h2>
                 <p className='md:text-lg text-base' data-aos="fade-up" data-aos-delay="300">Announcing our innovative generative AI service. Discover the future of content creation with our Generative AI! This innovative technology enables us to create high-quality, unique text, photos, and videos that are personalized according to your specific requirements. Enhance your engagement with creative, engaging content. Explore the possibilities now!.</p>
-                <Link href={"/service/emerging-technology"}>
-                    <div data-aos="fade-up" data-aos-delay="300" className='md:px-10 px-5 md:py-3 py-1 cursor-pointer w-fit font-medium bg-sky-700 transition-all text-white md:text-lg text-base  rounded-lg shadow-lg'>
+                <Link href={"/service/servicenow-implementation-and-support"}>
+                    <div data-aos="fade-up" data-aos-delay="300" className='md:px-10 px-5 md:py-3 py-1 cursor-pointer w-fit font-medium bg-sky-700 transition-all text-white md:text-lg text-base rounded-lg shadow-lg'>
                         Artificial Intelligent Services
                     </div>
                 </Link>
